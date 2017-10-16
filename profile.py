0b1214a58646f2242bb5b27cf01d2984a4462128
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-10-16 13:25:39 -0700

    Changed README and profile.py comments.
    Updated comments and README to reflect that this profile is for starting
    up a cluster with an NFS mounted dataset.

--- profile.py
+++ profile.py
@@ -1,15 +1,10 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source
-code and other software, documentation, etc. are stored in in a publicly
-accessible GIT repository (say, github.com). When you instantiate this profile,
-the repository is cloned to all of the nodes in your experiment, to
-`/local/repository`. 
+"""
+This profile mounts an Emulab hosted dataset on a number of servers through an
+NFS server. 
 
-This particular profile is a simple example of using a single raw PC. It can be
-instantiated on any cluster; the node will boot the default operating system,
-which is typically a recent version of Ubuntu.
-
-Instructions: Wait for the profile instance to start, then click on the node in
-the topology and choose the `shell` menu item.  """
+Instructions: 
+The dataset is mounted on /mnt/dataset on the client nodes.
+"""
 
 # Import the Portal object.
 import geni.portal as portal
